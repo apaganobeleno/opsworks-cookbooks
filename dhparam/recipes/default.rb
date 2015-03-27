57f9367092bd8b74a6fbbefdb2787dd0abd2568a
Tunde Agboola <tun.agboola@gmail.com>
2015-03-27 16:59:12 -0400

    small change to dhparam recipe

--- dhparam/recipes/default.rb
+++ dhparam/recipes/default.rb
@@ -2,7 +2,7 @@
 # Recipe:: default
 # Generate DHE key
 
-execute "Generate DHE Keys" do
+execute 'Generating 2048-bit DHE Keys' do
   user 'root'
   command 'openssl dhparam -out dhparam.pem 2048'
   cwd '/etc/ssl/certs'
